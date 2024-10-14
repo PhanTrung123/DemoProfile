76b7357e933c6c213dba597a3e92cb9c2dbc6af3
PhanTrung123 <phanquangtrung30042004@gmail.com>
2024-10-14 10:57:28 +0700

    init project

---
+++ script.js
@@ -0,0 +1,6 @@
+function loggerMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".info-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
